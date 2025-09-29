2667e24e64824d2de4efbf6f6ae9cc5d535ce41d
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-29 11:22:49 -0300

    cotroller y rutas de user

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,17 @@
-export const getAllUsers = async (_req, res) => {
+import { UserModel } from "../models/mongoose/user.model.js";
+
+export const getAllUsers = async (req, res) => {
   try {
     // TODO: devolver usuarios con profile y sus assets con sus categories (populate) (solo admin)
+    const users = await UserModel.find().populate({
+      path: "assets",
+      select: "inventoryNumber description status",
+      populate: {
+        path: "categories",
+        select: "name",
+      },
+    });
+    console.log(req.user);
     return res.status(200).json({ data: users });
   } catch (error) {
     console.log(error);
@@ -9,9 +20,17 @@ export const getAllUsers = async (_req, res) => {
 };
 
 export const deleteUser = async (req, res) => {
+  const { _id } = req.params;
   try {
     // TODO: eliminación lógica (deletedAt) (solo admin)
-    return res.status(204).json({ msg: "Usuario eliminado correctamente" });
+    const deletedUser = await UserModel.findOneAndUpdate(
+      _id,
+      { deletedAt: new Date() },
+      { new: true }
+    );
+    return res
+      .status(204)
+      .json({ msg: "Usuario eliminado correctamente.", data: deletedUser });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Error interno del servidor" });
