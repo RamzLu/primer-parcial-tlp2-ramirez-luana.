596803290ad0da36ef155da936d7ca6003151997
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-29 11:07:20 -0300

    payload de sign token

--- src/helpers/jwt.helper.js
+++ src/helpers/jwt.helper.js
@@ -1,8 +1,19 @@
 import jwt from "jsonwebtoken";
 
 // * funcion para crear un token con una firma secreta y un tiempo de expiración
-export const signToken = (payload) =>
-  jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+export const signToken = (user) =>
+  jwt.sign(
+    {
+      id: user.id,
+      role: user.role,
+      first_name: user.profile.first_name,
+      last_name: user.profile.last_name,
+      employee_number: user.profile.employee_number,
+      phone: user.profile.phone,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
 
 // * funcion para verificar que un token este con la misma firma secreta
 export const verifyToken = (token) => jwt.verify(token, process.env.JWT_SECRET);
