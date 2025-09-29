b692518598aa955194dd8f2ccb70c6a256d559fc
Ramirez Luana <luanaabigail168@gmail.com>
2025-09-29 10:44:06 -0300

    login y register hechos

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,20 @@
+import { comparePassword } from "../helpers/bcrypt.helper.js";
+import { hashPassword } from "../helpers/bcrypt.helper.js ";
+import { signToken } from "../helpers/jwt.helper.js";
+import { UserModel } from "../models/mongoose/user.model.js";
+
 export const register = async (req, res) => {
+  const { username, email, password, role, profile } = req.body;
   try {
     // TODO: crear usuario con password hasheada y profile embebido
+    const hashedPassword = await hashPassword(password);
+    await UserModel.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
+      role: role,
+      profile: profile,
+    });
     return res.status(201).json({ msg: "Usuario registrado correctamente" });
   } catch (error) {
     console.log(error);
@@ -9,8 +23,32 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
+  const { email, password } = req.body;
   try {
     // TODO: buscar user, validar password, firmar JWT y setear cookie httpOnly
+    const user = await UserModel.findOne({
+      email: email,
+    });
+    if (!user) {
+      return res.status(404).json({
+        msg: "El usuario o la contraseña no coinciden",
+      });
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(404).json({
+        msg: "El usuario o la contraseña no coinciden",
+      });
+    }
+
+    const token = signToken(user);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60,
+    });
+
     return res.status(200).json({ msg: "Usuario logueado correctamente" });
   } catch (error) {
     console.log(error);
